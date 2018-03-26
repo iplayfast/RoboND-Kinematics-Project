4db15e738075d1128bce4c5f9f043f4728bc247a
Chris Bruner <cwbruner@gmail.com>
2018-03-25 23:31:01 -0400

    Update Project submission

--- kuka_arm/scripts/IK_server.py
+++ kuka_arm/scripts/IK_server.py
@@ -43,25 +43,31 @@ def handle_calculate_IK(req):
 
             
             # Create Modified DH parameters
-        DH_Table = { linkTwist0:0.0,    linkLength0:0.0,    linkOffset1:0.75, joint1:joint1,
-                     linkTwist1:-pi/2., linkLength1:0.35,   linkOffset2:0.0, joint2:-pi/2.0 + joint2,
-                     linkTwist2:0.0,    linkLength2:1.25,   linkOffset3:0.0,  joint3:joint3,
-                     linkTwist3:-pi/2.0,linkLength3:-0.054, linkOffset4:1.5,  joint4:joint4,
-                     linkTwist4:pi/2.0, linkLength4:0.0,    linkOffset5:0.0,  joint5:joint5,
-                     linkTwist5:-pi/2.0,linkLength5:0.0,    linkOffset6:0.0,  joint6:joint6,
-                     linkTwist6:0.0,    linkLength6:0.0, linkOffset7:0.303,joint7:0.0 }
+        DH_Table = { linkTwist0:   0.0, linkLength0:   0.0, linkOffset1: 0.75, joint1:  joint1,
+                     linkTwist1:-pi/2., linkLength1:  0.35, linkOffset2:  0.0, joint2: -pi/2.0 + joint2,
+                     linkTwist2:   0.0, linkLength2:  1.25, linkOffset3:  0.0, joint3:  joint3,
+                     linkTwist3:-pi/2.0,linkLength3:-0.054, linkOffset4:  1.5, joint4:  joint4,
+                     linkTwist4: pi/2.0,linkLength4:   0.0, linkOffset5:  0.0, joint5:  joint5,
+                     linkTwist5:-pi/2.0,linkLength5:   0.0, linkOffset6:  0.0, joint6:  joint6,
+                     linkTwist6:    0.0,linkLength6:   0.0, linkOffset7:0.303, joint7:  0.0 }
             #
             #
             # Define Modified DH Transformation matrix
-       T0_1 = TF_Matrix(linkTwist0, linkLength0, linkOffset1,joint1).subs(DH_Table)
+        T0_1 = TF_Matrix(linkTwist0, linkLength0, linkOffset1,joint1).subs(DH_Table)
         T1_2 = TF_Matrix(linkTwist1, linkLength1, linkOffset2,joint2).subs(DH_Table)
         T2_3 = TF_Matrix(linkTwist2, linkLength2, linkOffset3,joint3).subs(DH_Table)
         T3_4 = TF_Matrix(linkTwist3, linkLength3, linkOffset4,joint4).subs(DH_Table)
         T4_5 = TF_Matrix(linkTwist4, linkLength4, linkOffset5,joint5).subs(DH_Table)
         T5_6 = TF_Matrix(linkTwist5, linkLength5, linkOffset6,joint6).subs(DH_Table)
         T6_EE =TF_Matrix(linkTwist6, linkLength6, linkOffset7,joint7).subs(DH_Table)
-        T0_EE = T0_1 * T1_2 * T2_3 * T3_4 * T4_5 * T5_6 * T6_EE;
+        T0_EE = T0_1 * T1_2 * T2_3 * T3_4 * T4_5 * T5_6 * T6_EE
      #
+
+        #correction to orientation of Gripper in UDRF vs DH
+        R_corr = Matrix([[0,0,1.0,0],[0,-1.0,0,0],[1.0,0,0,0],[0,0,0,1.0]])
+        # apply correction to result
+        T0_EE_corr = (T0_EE * R_corr)
+
             #
             # Create individual transformation matrices
             #
@@ -69,10 +75,25 @@ def handle_calculate_IK(req):
             # Extract rotation matrices from the transformation matrices
             #
             #
-            ###
-        R0_1 = T0_1[0:3,0:3]
-        R1_2 = T1_2[0:3,0:3]
-        R2_3 = T2_3[0:3,0:3]
+              # Initialize service response
+        joint_trajectory_list = []
+        for x in xrange(0, len(req.poses)):
+            # IK code starts here
+            joint_trajectory_point = JointTrajectoryPoint()
+
+	    # Extract end-effector position and orientation from request
+	    # px,py,pz = end-effector position
+	    # roll, pitch, yaw = end-effector orientation
+            px = req.poses[x].position.x
+            py = req.poses[x].position.y
+            pz = req.poses[x].position.z
+
+            (roll, pitch, yaw) = tf.transformations.euler_from_quaternion(
+                [req.poses[x].orientation.x, req.poses[x].orientation.y,
+                    req.poses[x].orientation.z, req.poses[x].orientation.w])
+
+            ### Your IK code here
+    ###
         # extract end-effector position and orientation from request
         # px,py,pz = end-effector position
         # roll, pitch, yaw = end-effector orientation
@@ -85,19 +106,19 @@ def handle_calculate_IK(req):
                  req.poses[x].orientation.z, req.poses[x].orientation.w]);
 
         r,  p,  y = symbols('r p y')
-        ROT_x = Matrix([[1,0,0],
+        R_roll = Matrix([[1,0,0],
                         [0,cos(r), -sin(r)],
                         [0,sin(r), cos(r)]])    #roll
-        ROT_y = Matrix([[cos(p),    0,  sin(p)  ],
+        R_pitch = Matrix([[cos(p),    0,  sin(p)  ],
                         [0,         1,          0],
                         [-sin(p),   0,  cos(p)]])   #pitch
-        ROT_z = Matrix([[cos(y), -sin(y), 0],
+        R_yaw = Matrix([[cos(y), -sin(y), 0],
                         [sin(y),    cos(y), 0],
                         [0, 0,  1]])        #yaw
 
-        ROT_EE = ROT_z * ROT_y * ROT_x
+        ROT_EE = R_yaw * R_pitch * R_roll
 
-        Rot_Error = ROT_z.subs(y, radians(180)) * ROT_y.subs(p,radians(-90))
+        Rot_Error = R_yaw.subs(y, radians(180)) * R_pitch.subs(p,radians(-90))
 
         ROT_EE = ROT_EE * Rot_Error
 
@@ -127,25 +148,7 @@ def handle_calculate_IK(req):
         linkLength5 = atan2(sqrt(R3_6[0,2]*R3_6[0,2] + R3_6[2,2]*R3_6[2,2]),R3_6[1,2])
         linkLength6 = atan2(-R3_6[1,1],R3_6[1,0])
 
-        # Initialize service response
-        joint_trajectory_list = []
-        for x in xrange(0, len(req.poses)):
-            # IK code starts here
-            joint_trajectory_point = JointTrajectoryPoint()
-
-	    # Extract end-effector position and orientation from request
-	    # px,py,pz = end-effector position
-	    # roll, pitch, yaw = end-effector orientation
-            px = req.poses[x].position.x
-            py = req.poses[x].position.y
-            pz = req.poses[x].position.z
-
-            (roll, pitch, yaw) = tf.transformations.euler_from_quaternion(
-                [req.poses[x].orientation.x, req.poses[x].orientation.y,
-                    req.poses[x].orientation.z, req.poses[x].orientation.w])
-
-            ### Your IK code here
-	    # Compensate for rotation discrepancy between DH parameters and Gazebo
+  	    # Compensate for rotation discrepancy between DH parameters and Gazebo
 	    #
 	    #
 	    # Calculate joint angles using Geometric IK method
@@ -155,8 +158,8 @@ def handle_calculate_IK(req):
 
             # Populate response for the IK request
             # In the next line replace linkLength1,linkLength2...,linkLength6 by your joint angle variables
-	    joint_trajectory_point.positions = [linkLength1, linkLength2, linkLength3, linkLength4, linkLength5, linkLength6]
-	    joint_trajectory_list.append(joint_trajectory_point)
+	joint_trajectory_point.positions = [linkLength1, linkLength2, linkLength3, linkLength4, linkLength5, linkLength6]
+	joint_trajectory_list.append(joint_trajectory_point)
 
         rospy.loginfo("length of Joint Trajectory List: %s" % len(joint_trajectory_list))
         return CalculateIKResponse(joint_trajectory_list)
